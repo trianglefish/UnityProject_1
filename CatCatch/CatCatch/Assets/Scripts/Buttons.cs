359c953d83b6f82aeb78af01e03307736c1756c4
d.pinhcuk <pincukdn@gmail.com>
2016-05-24 00:01:18 +0300

    2016-05-24 cat cat

---
+++ CatCatch/CatCatch/Assets/Scripts/Buttons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public Sprite buttonPressed, buttonReleased;
+	public string action; //переменная для определения действий пользователя
+
+	void OnMouseDown()
+	{
+		GetComponent <SpriteRenderer> ().sprite = buttonPressed;
+	}
+
+	void OnMouseUp()
+	{
+		GetComponent <SpriteRenderer> ().sprite = buttonReleased;
+	}
+
+	void OnMouseAsButton () //функция, которая проверяетя, чтобы кнопка нажималась в пределах коллайдера
+	{
+		switch (gameObject.name) {
+		case "playButton":
+			Application.LoadLevel (1);
+			break;
+		case "information":
+			Application.OpenURL ("http://google.com");
+			print ("где ссыль блять");
+            break;
+		}
+	}
+	 
+}
